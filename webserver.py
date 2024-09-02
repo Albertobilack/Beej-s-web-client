86e8032b470e2c8d38bb399cdd31a3dfc90e6cb4
Alberto Bino <binoalberto02@gmail.com>
2024-09-03 00:43:57 +0200

    implemented challenges from cap. 5.7 Extensions

--- webserver.py
+++ webserver.py
@@ -3,6 +3,12 @@
 
 import socket
 import sys
+import signal
+import re
+
+def signalHandler(sig, frame):
+    print("SERVER SHUTDOWN")
+    sys.exit(0)
 
 if __name__ == "__main__":
 
@@ -16,29 +22,56 @@ if __name__ == "__main__":
         #address = ('', server.bind(28333))
         server.bind(('', 28333))
 
-    print(server)
+    #print(server)
 
     #server.bind(address)
 
     server.listen()
     while True:
         newConnection = server.accept() #tupla socket + return address
-        #print("NUOVA CONNESSIONE")
-        #print(newConnection)
+        print("connected with IP address: {} | client port number: {}".format(newConnection[1][0], newConnection[1][1]))
         newSocket = newConnection[0]
         # newSocket.connect(newConnection[1]) non necessario, socket si connette automaticamente
         #request = b''
         request = ""
+        byteScanned, payloadByteLenght = 0, 0
+        readingPayload = False
         while True:
+            if readingPayload and byteScanned>=payloadByteLenght:
+                break
             buffer = newSocket.recv(4096)
             if not buffer:
-                print("connessione chiusa")
+                print("the connection to the client has ended")
                 break
             request += buffer.decode("ISO-8859-1")
-            if "\r\n\r\n" in request:
-                break
-        #print(request)
-        response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Lenght: 6\r\nConnection: close\r\n\r\nHello!\r\n"
+            if readingPayload:
+                byteScanned += len(buffer)
+            if "\r\n\r\n" in request and not byteScanned:       #it's possibile to avoid a research in the whole request searching ( i-1Buffer + iBuffer )
+                requestData = request.split("\r\n")
+                for data in requestData:
+                    if "Content Lenght:" in data:
+                        match = re.search(r'\d+', data)
+                        if match:
+                            payloadByteLenght = int(match.group(0))
+                            print("byte of payload:", payloadByteLenght)
+                            readingPayload = True
+                            initialPayload = request.split("\r\n\r\n")
+                            byteScanned += len(initialPayload[1].encode("ISO-8859-1"))
+                            print("byte iniziali: ", byteScanned)
+                            break
+                        else:
+                            print("no payload lenght provided")
+
+        requestMethod = request.split("/") #there should be a bettere way to do this
+        print("method: ", requestMethod[0].lstrip())
+        payload = request.split("close\r\n\r\n")
+        if payload[1]:
+            print("payload: ", payload[1], "\n")
+        else:
+            print("no payload", "\n")
+
+        response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Lenght: 6\r\nConnection: close\r\n\r\nPayload recived, thanks\r\n"
         newSocket.sendall(response.encode("ISO-8859-1"))
         newSocket.close()
+        signal.signal(signal.SIGINT, signalHandler)
 
