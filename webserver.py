1767c99d597c3806434714e76cd24a86a34d1d67
Alberto Bino <binoalberto02@gmail.com>
2024-09-03 02:44:49 +0200

    improved client handling
    improved client handling logic, need some work tho

--- webserver.py
+++ webserver.py
@@ -1,6 +1,3 @@
-#todo: payload handle = The right thing to do would be to look for a
-#      Content-Length header and then receive the header plus that many bytes. 
-
 import socket
 import sys
 import signal
@@ -12,66 +9,77 @@ def signalHandler(sig, frame):
 
 if __name__ == "__main__":
 
+    serverAddress = ('', 28333)
+    response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Lenght: 24\r\nConnection: close\r\n\r\nPayload recived, thanks\r\n"
+    
+    #set custom port for server if provided from command line
+    if len(sys.argv) == 2:
+        serverAddress[1] = int(sys.argv[1])
+
+    #server setup
     server = socket.socket()
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-    if len(sys.argv) == 2:
-        #address = ('', sys.argv[1])
-        server.bind(('', int(sys.argv[1])))
-    else:
-        #address = ('', server.bind(28333))
-        server.bind(('', 28333))
-
-    #print(server)
-
-    #server.bind(address)
-
+    server.bind(serverAddress)
     server.listen()
+
+    #client handling
     while True:
         newConnection = server.accept() #tupla socket + return address
         print("connected with IP address: {} | client port number: {}".format(newConnection[1][0], newConnection[1][1]))
         newSocket = newConnection[0]
-        # newSocket.connect(newConnection[1]) non necessario, socket si connette automaticamente
-        #request = b''
-        request = ""
+        header = ""                    #header = b''
+        payload = ""
         byteScanned, payloadByteLenght = 0, 0
-        readingPayload = False
+
         while True:
-            if readingPayload and byteScanned>=payloadByteLenght:
+            buffer = newSocket.recv(4096)
+            if not buffer:  #disconnection handling
+                print("the connection to the client has ended")
+                break
+            header += buffer.decode("ISO-8859-1")
+            if "\r\n\r\n" in header:
+                initialPayload = header.split("\r\n\r\n")
+                for splits in initialPayload[1:]:   #marginale, in caso payload passe il carattere specifico \r\n\r\n
+                    payload += splits
+                header = initialPayload[0]
+                #search for content lenght
+                for headerData in header.split("\r\n"):
+                    if "Content Lenght:" in headerData:
+                        match = re.search(r'\d+', headerData)
+                        if match:
+                            payloadByteLenght = int(match.group(0))
+                            byteScanned += len(initialPayload[1].encode("ISO-8859-1"))
+                            print("byte iniziali: ", byteScanned)
+                            print("byte of payload:", payloadByteLenght)
+                            break
+                        else:
+                            print("no payload lenght provided")
+                            break
+                else:
+                    continue
+                break
+
+        while True:
+            if byteScanned>=payloadByteLenght:
                 break
             buffer = newSocket.recv(4096)
             if not buffer:
                 print("the connection to the client has ended")
                 break
-            request += buffer.decode("ISO-8859-1")
-            if readingPayload:
-                byteScanned += len(buffer)
-            if "\r\n\r\n" in request and not byteScanned:       #it's possibile to avoid a research in the whole request searching ( i-1Buffer + iBuffer )
-                requestData = request.split("\r\n")
-                for data in requestData:
-                    if "Content Lenght:" in data:
-                        match = re.search(r'\d+', data)
-                        if match:
-                            payloadByteLenght = int(match.group(0))
-                            print("byte of payload:", payloadByteLenght)
-                            readingPayload = True
-                            initialPayload = request.split("\r\n\r\n")
-                            byteScanned += len(initialPayload[1].encode("ISO-8859-1"))
-                            print("byte iniziali: ", byteScanned)
-                            break
-                        else:
-                            print("no payload lenght provided")
+            payload += buffer.decode("ISO-8859-1")
 
-        requestMethod = request.split("/") #there should be a bettere way to do this
+
+        requestMethod = header.split("/") #there should be a bettere way to do this
         print("method: ", requestMethod[0].lstrip())
-        payload = request.split("close\r\n\r\n")
-        if payload[1]:
-            print("payload: ", payload[1], "\n")
+        if payload:
+            print("payload: ", payload, "\n")
         else:
             print("no payload", "\n")
 
-        response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Lenght: 6\r\nConnection: close\r\n\r\nPayload recived, thanks\r\n"
+        #send payload to client and close socket dediated to client 
         newSocket.sendall(response.encode("ISO-8859-1"))
         newSocket.close()
+
+        #shut down server if CTRL + C
         signal.signal(signal.SIGINT, signalHandler)
 
